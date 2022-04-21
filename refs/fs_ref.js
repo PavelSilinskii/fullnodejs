073d9ab66bc0f6dd735bf987251c0ae21064bcf1
Pavel <work322@gmail.com>
2022-04-22 00:20:46 +0300

    Was learning module os

--- refs/fs_ref.js
+++ refs/fs_ref.js
@@ -2,49 +2,49 @@ const fs = require('fs')
 const path = require('path')
 
 //file sistem
-// console.log(path.join(__dirname,'note'))
-//  fs.mkdir(path.join(__dirname,'notes'), err => {
+console.log(path.join(__dirname,'note'))
+ fs.mkdir(path.join(__dirname,'notes'), err => {
 
-// if (err) throw new Error(err)
+if (err) throw new Error(err)
 
-//      if (err) throw err
+     if (err) throw err
 
-//      console.log('dir was created')
-//  })
+     console.log('dir was created')
+ })
 
-// fs.writeFile(path.join(__dirname,'notes','mynotes.txt'),
-// 'Hello world',
-//  (err)=>{
-//     if(err) throw new Error(err)
-//     console.log('The file was created') 
+fs.writeFile(path.join(__dirname,'notes','mynotes.txt'),
+'Hello world',
+ (err)=>{
+    if(err) throw new Error(err)
+    console.log('The file was created') 
 
-//     fs.appendFile(
-//         path.join(__dirname, 'notes', 'mynotes.txt'),
-//         ' From append file',
-//         (err) =>{
-//             if (err) throw err
-//             console.log('The file was updated')
-//         }
-//     )
-// })
+    fs.appendFile(
+        path.join(__dirname, 'notes', 'mynotes.txt'),
+        ' From append file',
+        (err) =>{
+            if (err) throw err
+            console.log('The file was updated')
+        }
+    )
+})
 
 
-// fs.readFile(
-//     path.join(__dirname, 'notes', 'mynotes.txt'),
-//     'utf-8',
-//     (err,data) => {
-//         if (err) throw err
-//         console.log(data)
-//     }
-// )
-
-fs.rename(
+fs.readFile(
     path.join(__dirname, 'notes', 'mynotes.txt'),
-    path.join(__dirname, 'notes','notes.txt'),
-    (err) =>{
+    'utf-8',
+    (err,data) => {
         if (err) throw err
-
-        console.log('The file was renamed')
-
+        console.log(data)
     }
 )
+
+// fs.rename(
+//     path.join(__dirname, 'notes', 'mynotes.txt'),
+//     path.join(__dirname, 'notes','notes.txt'),
+//     (err) =>{
+//         if (err) throw err
+
+//         console.log('The file was renamed')
+
+//     }
+// )
