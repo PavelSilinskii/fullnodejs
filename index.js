8d8eced2e7a5610798b5c9898b77f55fd17cf419
Pavel <work322@gmail.com>
2022-04-23 01:01:47 +0300

    I Learned the http module

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-// console.log('Hello',__dirname)
-// console.log('Hello',__filename)
-const userObj = require ('./user')
-console.log(userObj.user)
-userObj.sayHello()
+// const userObj = require ('./user')
+// console.log(userObj.user)
+// userObj.sayHello()
+
+const http = require ('http')
+
+const server = http.createServer(
+    (req,res) => {
+console.log(req.url)
+
+res.write('<h1>hello from NodeJs</h1>')
+res.write('<h2>hello from NodeJs</h2>')
+res.write('<h3>hello from NodeJs</h3>')
+res.end(`
+<div style="background:red; width:200px; height:200px">
+<h1>Test nodeJs</h1>
+</div>
+`)
+    }
+)
+
+server.listen(3000, ()=>{
+    console.log('Server was runing...')
+})
