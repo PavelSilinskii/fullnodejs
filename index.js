6114102d3df152c94c0044103e9f378752ed6640
Pavel <work322@gmail.com>
2022-04-25 00:05:48 +0300

    create easy web server

--- index.js
+++ index.js
@@ -4,20 +4,42 @@
 
 const http = require ('http')
 
-const server = http.createServer(
-    (req,res) => {
-console.log(req.url)
+const server = http.createServer((req,res) => {
+     if(req.method==='GET'){
 
-res.write('<h1>hello from NodeJs</h1>')
-res.write('<h2>hello from NodeJs</h2>')
-res.write('<h3>hello from NodeJs</h3>')
-res.end(`
-<div style="background:red; width:200px; height:200px">
-<h1>Test nodeJs</h1>
-</div>
-`)
-    }
-)
+      res.writeHead(200,{
+          'Content-Type':'text/html'
+      })  
+    
+      res.end(`
+      <h1>Form</h1>
+      <form method='post' action='/'>
+      <input name ='title' type ='text'>
+      <button type='submit'> Send</button>
+      </form>
+      `)   
+     }else if(req.method ==='POST'){
+        const body = []
+        res.writeHead(200,{
+           'Content-Type' : 'text/html; charset=utf-8' 
+        })
+
+        req.on('data', (data)=>{
+          body.push(Buffer.from(data))  
+        })
+
+        req.on('end', ()=>{
+const message = body.toString().split('=')[1]
+        
+        
+        res.end(`
+        <h1> Ваше сообщение: ${message}</h1>
+        `)
+    })
+        
+     }
+    
+    })
 
 server.listen(3000, ()=>{
     console.log('Server was runing...')
